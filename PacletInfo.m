6f20653b70d5baad2a870dd4f7d1116403ff16b4
b3m2a1 <b3m2a1@gmail.com>
2017-10-19 00:00:34 -0700

    Committed AmhCode application @ 2017-10-19_00:00:34

--- PacletInfo.m
+++ PacletInfo.m
@@ -1,4 +1,7 @@
 (* Created with the Wolfram Language : www.wolfram.com *)
-Paclet[Name -> "AmhCode", Version -> "1.0.0", 
+Paclet[Location -> 
+  "/Users/Mark/Documents/Wolfram Mathematica/Applications/AmhCode", 
+ Name -> "AmhCode", Version -> "1.0.1", 
  Extensions -> {{"Documentation", "Language" -> "English"}, 
-   {"Kernel", Root -> ".", Context -> {"AmhCode`"}}, {"FrontEnd"}}]
+   {"Kernel", "Root" -> ".", "Context" -> {"AmhCode`"}, Root -> ".", 
+    Context -> {"AmhCode`"}}, {"FrontEnd"}}]
