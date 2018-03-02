25d317adcd7e8aef58a814d29917c5d9a6dfea98
b3m2a1 <b3m2a1@gmail.com>
2018-03-02 00:10:05 -0800

    Commited via Mathematica
    Committed AmhCode application @ 2018-03-02_00:10:05

--- PacletInfo.m
+++ PacletInfo.m
@@ -1,9 +1,8 @@
 (* Created with the Wolfram Language : www.wolfram.com *)
-Paclet[Location -> 
-  "/Users/Mark/Documents/Wolfram Mathematica/Applications/AmhCode", 
- Name -> "AmhCode", Version -> "1.0.3", Creator -> "b3m2a1@gmail.com", 
+Paclet[Name -> "AmhCode", Version -> "1.0.4", Creator -> "b3m2a1@gmail.com", 
  Description -> "A paclet full of legacy code from a few years ago.\n Mostly \
-depends on a python-style object system.", 
+depends on a python-style object system.", WolframVersion -> "10+", 
+ Internal -> False, SystemID -> All, Tags -> {"mathematica"}, 
  Extensions -> {{"Kernel", "Root" -> ".", "Context" -> {"AmhCode`"}, 
     Root -> ".", Context -> {"AmhCode`"}}, {"Documentation", 
-    "Language" -> "English"}, {"FrontEnd"}}, Tags -> {"mathematica"}]
+    "Language" -> "English"}, {"FrontEnd"}}]
